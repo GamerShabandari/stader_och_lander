ac5fbe55cfc0e9d43991acf1429f61059df27b5a
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2021-12-22 15:59:57 +0100

    testar fixa meny

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 
+const navBar = document.getElementById("navBar");
+const mainContainer = document.getElementById("mainContainer");
 
+Promise.all([
+    fetch("land.json").then(response => response.json()),
+    fetch("stad.json").then(response => response.json()),
+
+])
+.then(jsonData => {
+   
+    let countries = jsonData[0];
+    let cities = jsonData[1];
+    renderNavbar(countries,cities);
+});
+
+function renderNavbar (countries, cities) {
+    
+
+    for (let i = 0; i < countries.length; i++) {
+        const navbarCountrie = countries[i];
+        console.log(navbarCountrie);
+        const menuDiv = document.createElement("div");
+        menuDiv.id = navbarCountrie.id;
+        menuDiv.innerText = navbarCountrie.countryname;
+
+        navBar.append(menuDiv);
+        
+    }
+
+
+
+};
